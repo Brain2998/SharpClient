df3296b7c4d042ebf21c26810b7f45a8ae76cb34
brain2998 <an.sereb96@gmail.com>
2018-10-19 14:48:46 +0300

    Before form transfer

---
+++ SharpClient/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace SharpClient
+{
+    public class Connection
+    {
+		private TcpClient tcpServer;
+        private StreamReader clientReader;
+        private StreamWriter clientWriter;
+
+		public Connection(IPAddress address, string Nickname)
+        {
+			tcpServer = new TcpClient();
+			tcpServer.Connect(address, 1986);
+			clientWriter = new StreamWriter(tcpServer.GetStream());
+            clientWriter.WriteLine(Nickname);
+            clientWriter.Flush();
+        }
+        public void CloseConnection()
+		{
+			tcpServer.Close();
+		}
+    }
+}
